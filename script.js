13ba7aa7843eca6acdb99a47920d1fc96a8037cf
Andy Bool <andybool50@gmail.com>
2024-10-23 15:22:19 +0100

    added new project and tinkered with styling

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ fetch('portfolio.json').then(function (res) {
     data.project.forEach(function (project) {
         const projectType = document.querySelector('.project')
         projectType.innerHTML += `
-    <div class="specificProject sm:basis-2/5 bg-[#90AEAD] max-w-2xl rounded-lg p-1.5">
+    <div class="specificProject mx-auto md:basis-2/5 bg-[#90AEAD] max-w-2xl rounded-lg p-1.5">
             <h2 class="p-2 font-bold bg-orange-600 text-orange-100 sm:text-3xl">${project.name}</h2>
         <div>
             <a href="${project.live}" title="See it live">
@@ -34,7 +34,7 @@ fetch('portfolio.json').then(function (res) {
         </div>
         <div class="projectText">
             <p class="pt-1.5 pb-3 text-cyan-900 font-semibold sm:text-3xl">${project.desc}</p>
-            <p class="py-2"><a class="bg-cyan-900 rounded-lg p-3 text-orange-100 sm:text-xl hover:bg-lime-300 hover:text-cyan-900 rounded-lg p-3" href="${project.url}" target="_blank">See it on github</a> | <a href="${project.live} "
+            <p class="py-2"><a class="bg-cyan-900 rounded-lg p-3 text-orange-100 sm:text-xl hover:bg-lime-300 hover:text-cyan-900 rounded-lg p-3" href="${project.url}" target="_blank">See it on <i class="fa-brands fa-github"></i></a> | <a href="${project.live} "
                     class="bg-cyan-900 rounded-lg p-3 text-orange-100 sm:text-xl hover:bg-lime-300 hover:text-cyan-900 rounded-lg p-3" >Live</a></p>
         </div>
     </div>`
